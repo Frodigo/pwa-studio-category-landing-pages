d029cf6fc91f9de09028d716c5ad0ae7038e425b
Marcin Kwiatkowski <contact@frodigo.com>
2020-10-07 08:52:59 +0200

    Add module base files

---
+++ src/@marcinkwiatkowski/category-landing-page/intercept.js
@@ -0,0 +1,12 @@
+const componentOverrideMapping = require('./componentOverrideMapping');
+const moduleOverridePlugin = require('./moduleOverrideWebpackPlugin');
+
+module.exports = targets => {
+    targets.of('@magento/pwa-buildpack').specialFeatures.tap(flags => {
+        flags[targets.name] = {esModules: true};
+    });
+
+    targets.of('@magento/pwa-buildpack').webpackCompiler.tap(compiler => {
+        new moduleOverridePlugin(componentOverrideMapping).apply(compiler);
+    })
+}
